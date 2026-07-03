51b5fadc3fff02407cfd0e4c717514c1a1726e4d
Sabari Kailas S <sabarikailasstc2025@gmail.com>
2026-07-03 10:28:58 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import os
+import glob
+
+# Handling MoviePy version difference(v1.x vs v2.x)
+try:
+  from moviepy.editor import ImageClip, concatenate_videoclips, AudioFileClip
+expect ImportError:
